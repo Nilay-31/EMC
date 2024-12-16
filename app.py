4d04c537e98481b691979db0ed61a7311e6e1379
Nilay-31 <nilaypatkar311@gmail.com>
2024-12-16 20:43:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
     if uploaded_file is not None:
        dt= pd.read_excel(uploaded_file) if uploaded_file.name.endswith('.xlsx') else pd.read_csv(uploaded_file)
         #st.write("Uploaded Dataset:")
-        st.write(dt.head())
+        #st.write(dt.head())
         
         if st.button("Predict"):
             # Load the saved model
