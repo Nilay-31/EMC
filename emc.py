c8db9070970c8d721909ad10b4ed7dd4379f603e
Nilay-31 <nilaypatkar311@gmail.com>
2024-12-16 19:56:25 +0530

    Update emc.py

--- emc.py
+++ emc.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ddQBe7bcKMJXapRk6N9c867RpbP6eDq5
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import seaborn as sns
