9bb06eacb70a0aef87302fc027383b7921d5dae0
Nilay-31 <nilaypatkar311@gmail.com>
2024-12-16 20:33:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import streamlit as st  # For deployment
+import streamlit as st# For deployment
+import pandas as pd
 def main():
     st.title("Email Marketing Campaign Success Predictor")
     st.write("Upload a file to analyze.")
@@ -6,19 +7,19 @@ def main():
     uploaded_file = st.file_uploader("Email_Marketing_Campaign_Dataset_Rounded.xlsx", type=["xlsx", "csv"])
     
     if uploaded_file is not None:
-        data = pd.read_excel(uploaded_file) if uploaded_file.name.endswith('.xlsx') else pd.read_csv(uploaded_file)
+       dt= pd.read_excel(uploaded_file) if uploaded_file.name.endswith('.xlsx') else pd.read_csv(uploaded_file)
         st.write("Uploaded Dataset:")
-        st.write(data.head())
+        st.write(dt.head())
         
         if st.button("Predict"):
             # Load the saved model
             loaded_model = joblib.load("email_campaign_model.pkl")
             
             # Make predictions
-            predictions = loaded_model.predict(data)
+            predictions = loaded_model.predict(dt)
             data['Predicted_Open'] = predictions
             st.write("Predicted Results:")
-            st.write(data)
+            st.write(dt)
 
 if __name__ == '__main__':
     main()
