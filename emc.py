89d66b139445212186685ed988994500dd931fe9
Nilay-31 <nilaypatkar311@gmail.com>
2024-12-20 16:07:24 +0530

    Update emc.py

--- emc.py
+++ emc.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
-Scaler-StandardScaler()
+Scaler=StandardScaler()
 
 def main():
     st.title('Email Marketing Campaign Success Predictor')
