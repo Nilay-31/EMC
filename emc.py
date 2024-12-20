78928dfaecc447870a472ed4c87d0ed079130e17
Nilay-31 <nilaypatkar311@gmail.com>
2024-12-20 16:06:50 +0530

    Update emc.py

--- emc.py
+++ emc.py
@@ -1,8 +1,11 @@
-import streamlit as st  # For deployment
-import pandas as pd  # For data handling
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler
+Scaler-StandardScaler()
 
 def main():
-    
     st.title('Email Marketing Campaign Success Predictor')
 
     # User input
